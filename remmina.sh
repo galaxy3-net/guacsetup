2fb255cc430679499c7f8f6fdd423f4ab7217112
Korben Kirscht <korben@kirscht.com>
2024-03-14 03:20:14 +0000

    Update remmina.sh

--- remmina.sh
+++ remmina.sh
@@ -1,3 +1,14 @@
 #!/bin/env bash
 
 sudo apt install -y remmina
+
+
+# Backup Remmina configs
+#   tar -czf ~/remmina_backup.tgz ~/.local/share/remmina/ ~/.config/remmina/ ~/.local/share/keyrings ~/.ssh/
+
+# Restore Remmina configs
+#   cd
+#   tar -xf ~/remmina_backup.tgz
+
+
+#  https://askubuntu.com/questions/420986/copy-or-export-remmina-remote-desktop-files-to-another-ubuntu-install
